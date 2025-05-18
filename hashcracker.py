1c8531124a12aca32433bfca39f5ea50937c9d02
MrGreenArrow <elias.poling@gmail.com>
2025-05-18 05:49:41 +0000

    Update hashcracker.py

--- hashcracker.py
+++ hashcracker.py
@@ -2,7 +2,8 @@ import hashlib
 import argparse
 
 # The goal here is to make a simple password hasher that would allow me to break simple passwords through brute-force attacks.
-def main():
+
+def password():
     parser = argparse.ArgumentParser(description="Password Cracker")
     parser.add_argument("--target", help="Target hash to crack", required=True)
     parser.add_argument("--algorithm", help="Hash algorithm (e.g. sha256)", required=True)
@@ -21,5 +22,21 @@ def main():
     except FileNotFoundError:
         print("Wordlist file not found.")
         
-if __name__ == "__main__":
-    main()
+if __name__ == "__password__":
+    password()
+
+while True:
+    print("What type of passcode would you like to crack?")
+    print("[1] - 4 to 6 digit PIN")
+    print("[2] - Password")
+    choice = input().strip()
+
+    if  choice == "1":
+        print("You have selected PIN mode")
+        break
+    elif choice == "2":
+        print("You have selected Password mode")
+        break
+    else:
+        print("Invalid input. Please select 1 or 2.")
+
